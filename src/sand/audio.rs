fae1d2c669937c161aa2b30aa5b69e18cefbbe66
James Sully <sullyj3@gmail.com>
2024-08-08 11:50:55 +1000

    create ElapsedSoundPlayer (WIP untested)

--- src/sand/audio.rs
+++ src/sand/audio.rs
@@ -1,10 +1,10 @@
 use std::convert::AsRef;
 use std::fmt::Debug;
 use std::io::{self, Read};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 
-use rodio;
+use rodio::{self, OutputStream, OutputStreamHandle, StreamError};
 
 // thanks sinesc
 // https://github.com/RustAudio/rodio/issues/141#issuecomment-383371609
@@ -29,7 +29,7 @@ impl Sound {
         Ok(Sound(Arc::from(buf)))
     }
 
-    pub fn cursor(self: &Self) -> io::Cursor<Sound> {
+    fn cursor(self: &Self) -> io::Cursor<Sound> {
         io::Cursor::new(self.clone())
     }
 
@@ -46,5 +46,78 @@ impl Sound {
             sink.sleep_until_end();
         });
         eprintln!("notification sound");
+    }
+}
+
+struct AudioPlayer {
+    stream: OutputStream,
+    handle: OutputStreamHandle,
+}
+
+impl AudioPlayer {
+    pub fn new() -> Result<Self, StreamError> {
+        let (stream, handle) = rodio::OutputStream::try_default()?;
+        Ok(Self { stream, handle })
+    }
+
+    pub fn play(&self, sound: &Sound) {
+        let decoder = sound.decoder();
+        let sink = rodio::Sink::try_new(&self.handle).unwrap();
+        sink.append(decoder);
+        sink.detach();
+    }
+}
+
+const SOUND_FILENAME: &str = "timer_sound.flac";
+
+fn xdg_sand_data_dir() -> Option<PathBuf> {
+    Some(dirs::data_dir()?.join("sand"))
+}
+
+fn xdg_sound_path() -> Option<PathBuf> {
+    Some(xdg_sand_data_dir()?.join(SOUND_FILENAME))
+}
+
+fn usrshare_sound_path() -> PathBuf {
+    Path::new("/usr/share/sand").join(SOUND_FILENAME)
+}
+
+pub struct ElapsedSoundPlayer {
+    sound: Sound,
+    player: AudioPlayer,
+}
+
+fn load_elapsed_sound() -> io::Result<Sound> {
+    if let Some(ref xdg_path) = xdg_sand_data_dir() {
+        let sound = Sound::load(xdg_path);
+        if sound.is_ok() {
+            return sound;
+        }
+    }
+    Sound::load(usrshare_sound_path())
+}
+
+#[derive(Debug)]
+enum ElapsedSoundPlayerError {
+    IO(io::Error),
+    Stream(StreamError),
+}
+
+impl ElapsedSoundPlayer {
+    pub fn load() -> Result<ElapsedSoundPlayer, ElapsedSoundPlayerError> {
+        use ElapsedSoundPlayerError as E;
+        let sound = load_elapsed_sound().map_err(|e| E::IO(e))?;
+        Self::new(sound).map_err(|e| E::Stream(e))
+    }
+
+    pub fn new(sound: Sound) -> Result<Self, StreamError> {
+        Ok(Self {
+            sound,
+            player: AudioPlayer::new()?
+        })
+    }
+
+    pub fn play(&self) {
+        self.player.play(&self.sound);
     }
 }
