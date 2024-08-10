1ddd5884600058d2eca79a173e36c39150e28bef
James Sully <sullyj3@gmail.com>
2024-08-11 01:06:23 +1000

    cleanup

--- src/daemon.rs
+++ src/daemon.rs
@@ -6,13 +6,8 @@ use std::mem;
 use std::os::fd::FromRawFd;
 use std::os::fd::RawFd;
 use std::os::unix;
-use std::path::Path;
-use std::path::PathBuf;
-use std::str;
-
 use async_scoped;
 use async_scoped::TokioScope;
-use dirs;
 use rodio::OutputStream;
 use tokio;
 use tokio::net::UnixListener;
@@ -24,24 +19,6 @@ use handle_client::handle_client;
 use ctx::DaemonCtx;
 
 const SYSTEMD_SOCKFD: RawFd = 3;
-const SOUND_FILENAME: &str = "timer_sound.opus";
-
-fn xdg_sand_data_dir() -> Option<PathBuf> {
-    Some(dirs::data_dir()?.join("sand"))
-}
-
-// TODO BUG return None if file does not exist
-fn xdg_sound_path() -> Option<PathBuf> {
-    Some(xdg_sand_data_dir()?.join(SOUND_FILENAME))
-}
-
-fn usrshare_sound_path() -> Option<PathBuf> {
-    Some(Path::new("/usr/share/sand").join(SOUND_FILENAME))
-}
-
-fn sound_path() -> Option<PathBuf> {
-    xdg_sound_path().or_else(|| usrshare_sound_path())
-}
 
 fn env_fd() -> Option<u32> {
     let str_fd = std::env::var("SAND_SOCKFD").ok()?;
@@ -50,6 +27,20 @@ fn env_fd() -> Option<u32> {
         .expect("Error: Found SAND_SOCKFD but couldn't parse it as a string")
         .into();
     Some(fd)
+}
+
+fn get_fd() -> RawFd {
+    match env_fd() {
+        None => {
+            eprintln!("SAND_SOCKFD not found, falling back on default.");
+            SYSTEMD_SOCKFD
+        }
+        Some(fd) => {
+            eprintln!("Found SAND_SOCKFD.");
+            fd.try_into()
+                .expect("Error: SAND_SOCKFD is too large to be a file descriptor.")
+        }
+    }
 }
 
 async fn accept_loop(listener: UnixListener, state: &DaemonCtx) {
@@ -70,8 +61,10 @@ async fn accept_loop(listener: UnixListener, state: &DaemonCtx) {
     }
 }
 
-async fn daemon(fd: RawFd) -> io::Result<()> {
-    eprintln!("daemon started.");
+async fn daemon() -> io::Result<()> {
+    eprintln!("Starting sand daemon {}", sand::VERSION);
+
+    let fd = get_fd();
 
     let o_handle = match OutputStream::try_default() {
         Ok((stream, handle)) => {
@@ -86,6 +79,7 @@ async fn daemon(fd: RawFd) -> io::Result<()> {
     std_listener.set_nonblocking(true)?;
     let listener: UnixListener = UnixListener::from_std(std_listener)?;
 
+    eprintln!("daemon started.");
     TokioScope::scope_and_block(|scope| {
         scope.spawn(accept_loop(listener, &state));
     });
@@ -94,25 +88,5 @@ async fn daemon(fd: RawFd) -> io::Result<()> {
 }
 
 pub fn main(_args: cli::DaemonArgs) -> io::Result<()> {
-    eprintln!("Starting sand daemon {}", sand::VERSION);
-
-    let fd: RawFd = match env_fd() {
-        None => {
-            eprintln!("SAND_SOCKFD not found, falling back on default.");
-            SYSTEMD_SOCKFD
-        }
-        Some(fd) => {
-            eprintln!("Found SAND_SOCKFD.");
-            fd.try_into()
-                .expect("Error: SAND_SOCKFD is too large to be a file descriptor.")
-        }
-    };
-
-    let o_sound_path = sound_path();
-    if o_sound_path.is_none() {
-        eprintln!("Warning: failed to locate notification sound. Audio will not work");
-    }
-
-    let rt = Runtime::new()?;
-    rt.block_on(daemon(fd))
+    Runtime::new()?.block_on(daemon())
 }
